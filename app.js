a9f7f570dcd619414eeb20fa94d8cffb5785eece
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-03-04 06:22:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answers = await inquirer.prompt({
+    name: "age",
+    type: "number",
+    message: "Enter your age"
+});
+console.log(chalk.yellow("I'll be " + (80 - answers.age) + "\n years in the coming 40 years! "));
